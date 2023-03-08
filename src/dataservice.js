73510b91934f4c82e1617471cc24ae2500e3b5b9
Arun Kumar Krishnan <emailarunkumar@gmail.com>
2023-03-08 01:39:53 -0800

    Updated API endpoints

--- src/dataservice.js
+++ src/dataservice.js
@@ -2,15 +2,15 @@ import DataService from './service';
 
 export const athletesService = {
   getLogin() {
-    return DataService.get('http://csika.org:8082/login');
+    return DataService.get('http://ec2-3-218-241-2.compute-1.amazonaws.com:8082/login');
   },
   getLoginAcess(url) {
     return DataService.get(url);
   },
   getAthletes() {
-    return DataService.get('http://csika.org:8082/athletes');
+    return DataService.get('http://ec2-3-218-241-2.compute-1.amazonaws.com:8082/athletes');
   },
   getActivites() {
-    return DataService.get('http://csika.org:8082/activities');
+    return DataService.get('http://ec2-3-218-241-2.compute-1.amazonaws.com:8082/activities');
   }
 };
